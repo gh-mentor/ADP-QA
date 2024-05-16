8a2450bc347f55fa33ed163f2c6e0c6aafa1533f
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-05-16 11:09:05 -0400

    fix product

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Product } from './Product';
 
 
-const product =  Product.createProduct
+const product =  Product.createProduct(1, 'Book', 19.99);
 
 // print the product details to the console
 console.log(product.toString());
